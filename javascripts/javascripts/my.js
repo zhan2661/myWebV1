56c36c3285832d13c71cd1e91a512bbcc386c940
steve <zhan2661@umn.edu>
2018-02-24 00:21:37 -0600

    Add files via upload

---
+++ javascripts/javascripts/my.js
@@ -0,0 +1,53 @@
+
+function car(){
+$('.carousel').carousel({
+            interval:false,
+        })
+
+}
+$(car);
+function roll(){
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+}
+$(roll);
+function nav(){
+var scrolling = false;
+$(window).on('scroll', function(){
+	if( !scrolling ) {
+		scrolling = true;
+		(!window.requestAnimationFrame)
+			? setTimeout(autoHideHeader, 250)
+			: requestAnimationFrame(autoHideHeader);
+	}
+});
+}
+$(nav);
+
+
+function toTop(){
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$("#intro").offset().top},"100");return false})})
+}
+
+$(toTop);
+
+function wa(){
+        new WOW().init();
+
+}
+
+$(wa);
